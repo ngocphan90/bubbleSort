56ca788ad6c0ea150a784dce6893b1e17de6b4d5
ngocphan90 <bichngoc0803@yahoo.com>
2021-03-12 20:26:49 -0600

    function to test cases  added

---
+++ src/funcBuildForTest.java
@@ -0,0 +1,13 @@
+public class funcBuildForTest {
+    //main function
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 1, 1, 4, 6};
+        int count = solution(nums);
+        System.out.println(count);
+    }
+
+    //sub-function
+    private static int solution(int[] nums) {
+        return 2;
+    }
+}
